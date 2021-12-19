283b23ea2514b2522d999091d2711a0b44cee220
CanonMukai <canon1white.rainbow@gmail.com>
2021-12-20 00:49:59 +0900

    Flamework

--- main.py
+++ main.py
@@ -3,6 +3,21 @@ from annealing import *
 
 # ページ全体の設定
 st.set_page_config(
-    page_title="HEMSデモ",
-    page_icon="🏠",
+    page_title='HEMSデモ',
+    page_icon='🏠',
+    layout='wide',
+    initial_sidebar_state='expanded',
 )
+
+def main():
+    make_body()
+    make_sidebar()
+
+def make_body():
+    st.text('Coming soon.')
+
+def make_sidebar():
+    st.sidebar.text('Coming soon.')
+
+if __name__ == '__main__':
+    main()
