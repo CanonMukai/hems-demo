6ed10d1f4ed84646a72ce395e69437b5f87061aa
CanonMukai <canon1white.rainbow@gmail.com>
2021-12-22 00:07:26 +0900

    Design: Make functions which create forms, buttons and take obj like sidebar or column

--- main.py
+++ main.py
@@ -17,21 +17,18 @@ st.set_page_config(
 # Page 関数
 ############################################
 
-def common():
-    # タイトル
-    st.title("HEMS エネルギー最適化")
-    # ページ遷移ボタン
-    for page in st.session_state.pages:
-        button = st.sidebar.button(
-            "{}へ".format(page.name),
-            key="button{}".format(page.name),
-            on_click=page.func,
-        )
+def create_transition_button(obj):
+    # obj に st は使えない
+    with obj:
+        for page in st.session_state.pages:
+            button = st.button(
+                "{}へ".format(page.name),
+                key="button{}".format(page.name),
+                on_click=page.func,
+            )
 
-def simple_demo_page():
-    common()
-    col1, col2, col3 = st.columns([1, 3, 2])
-    with col1.form("form"):
+def create_form(obj):
+    with obj.form("form"):
         tenki = st.selectbox(
             "天気",
             (
@@ -58,15 +55,31 @@ def simple_demo_page():
         submitted = st.form_submit_button("スケジューリング！")
         if submitted: pass
 
+def common_first():
+    # タイトル
+    st.title("HEMS エネルギー最適化")
+
+def common_last():
+    # ページ遷移ボタン
+    create_transition_button(st.sidebar)
+
+def simple_demo_page():
+    common_first()
+    col1, col2, col3 = st.columns([1, 3, 2])
+    create_form(col1)
     col2.write("アニーリングマシンの結果")
     col3.write("従来法の結果")
+    common_last()
 
 def detailed_demo_page():
-    common()
-    st.write("Coming soon.")
+    common_first()
+    col1, col2 = st.columns([2, 5])
+    create_form(col1)
+    col2.write("ここに結果を書くよ")
+    common_last()
 
 def explanation_page():
-    common()
+    common_first()
     st.markdown("""
 このページでは家庭におけるエネルギー利用のスケジューリングを
 アニーリングマシンで行う手法について解説します。
@@ -75,6 +88,7 @@ def explanation_page():
 """)
     st.latex("H = \sum_{i, j}JS_iS_j")
     st.write("※このプロジェクトは未踏ターゲット事務局によりサポートして頂いています。")
+    common_last()
 
 
 ############################################
