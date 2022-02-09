bafdb6e1c91ab364e2feb562982c576f696bdb55
CanonMukai <canon1white.rainbow@gmail.com>
2022-02-10 06:09:03 +0900

    return

--- event.py
+++ event.py
@@ -10,10 +10,29 @@ def event():
             'チョコ', 'チョコレート', '🍫', 'ショコラ', 'chocolate', 'ちょこ',
             'ちよこれーと', 'ちよこれいと']:
         display()
+        question()
 
 def display():
     st.write('お見事！！')
     st.image('https://drive.google.com/uc?export=view&id=1W0TtWXV5O-7emlBPv5OWE3VDBN9Gr1w6&usp=sharing')
+
+def question():
+    st.write('アンケートにご協力お願いいたします。')
+    with st.form('form'):
+        options = st.multiselect(
+            'どのようにして気づかれましたか？(複数回答可)',
+            options=[
+                '適当にいじってたら偶然見つけた',
+                'つよつよのダークモードを使っていてバレバレだった',
+                'なにかが妙だと思って探った',
+                'どれでもない',
+            ],
+            key='question')
+        st.form_submit_button(
+            label='送信',
+            on_click=st.session_state.pages['TOP'].func)
+    if 'question' in st.session_state:
+        st.write('ご協力ありがとうございました。')
 
 sukima1_text = 'はっ！お気づきになられましたか！'
 sukima2_text = 'ここもお気づきになられましたか！もう一息'
