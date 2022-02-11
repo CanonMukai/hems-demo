179370bd32620e0c4388a34a1f84eb1a903e8394
HemsQ <98304767+HemsQ@users.noreply.github.com>
2022-02-12 04:37:31 +0900

    Develop (#6)
    * hidden message
    
    * Change: HemsQ/hemsq.git
    
    * :)
    
    * [debug] :)
    
    * [debug] :)
    
    * [debug] :)
    
    * st.session_state_pages = {}
    
    * debug
    
    * [debug] transition
    
    * [debug] the position of css
    
    * [debug] display
    
    * [debug] display
    
    * [debug] maybe ok
    
    * return
    
    * delete question
    
    * css in top_page
    
    * change layout
    
    * unique key
    
    * text_css in write_white
    
    * modify
    
    * css for obj -> css for st
    
    * comment out head
    
    * delete head
    
    * only one css before text
    
    * delete css right after set_config
    
    * delete css at the fisrt part of top_page()
    
    * css right before write_white
    
    * colab button
    
    * Change button text
    
    * Change layout of top page
    
    * button func
    
    * side button css
    
    * [debug] modify css
    
    * nits
    
    * nits
    
    * nits
    
    * spinner message
    
    * request messages
    
    * nits
    
    * [debug]
    
    * [debug]
    
    * [debug]
    
    * nits
    
    Co-authored-by: CanonMukai <canon1white.rainbow@gmail.com>

--- event.py
+++ event.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import requests
+import json
 
 def event():
     st.text_input(
@@ -10,17 +12,34 @@ def event():
             'チョコ', 'チョコレート', '🍫', 'ショコラ', 'chocolate', 'ちょこ',
             'ちよこれーと', 'ちよこれいと']:
         display()
-        # question()
+        question()
 
 def display():
     st.write('お見事！！')
     st.image('https://drive.google.com/uc?export=view&id=1W0TtWXV5O-7emlBPv5OWE3VDBN9Gr1w6&usp=sharing')
 
+def submit():
+    options = ', '.join(st.session_state.question)
+    text = f'''
+＜どうやって？＞
+{options}
+
+＜メッセージ＞
+{st.session_state.event_message}
+'''
+    requests.post('https://hemsq-event.herokuapp.com/event',
+        headers={"content-type": "application/json"},
+        # data=json.dumps({'name': st.session_state.event_name, 'text': text}))
+        data=json.dumps({'name': '名無しさん', 'text': text}))
+    st.session_state.pages['TOP'].func()
+
 def question():
-    st.write('アンケートにご協力お願いいたします。')
+    st.write('もしよろしければ、アンケートにお答えください！')
     with st.form('question_form'):
-        options = st.multiselect(
-            'どのようにして気づかれましたか？(複数回答可)',
+        # name = st.text_input('ニックネーム', placeholder='なしでも大丈夫 :)',
+        #     key='event_name')
+        how = st.multiselect(
+            'どのようにして気づきましたか？(複数回答可)',
             options=[
                 '適当にいじってたら偶然見つけた',
                 'つよつよのダークモードを使っていてバレバレだった',
@@ -28,8 +47,12 @@ def question():
                 'どれでもない',
             ],
             key='question')
+        message = st.text_area('メッセージをもらえると主が嬉しくてニヤニヤします',
+            placeholder='もちろんなしでも大丈夫 :)',
+            key='event_message')
         st.form_submit_button(
             label='送信',
-            on_click=st.session_state.pages['TOP'].func)
+            on_click=submit,
+        )
 
 sukima_text = '...お？おおおおおお？'
