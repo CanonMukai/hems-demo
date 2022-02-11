1dc74949d9fe1200644f0172b697345d3e617da1
CanonMukai <canon1white.rainbow@gmail.com>
2022-02-12 03:09:15 +0900

    request messages

--- event.py
+++ event.py
@@ -16,10 +16,25 @@ def display():
     st.write('お見事！！')
     st.image('https://drive.google.com/uc?export=view&id=1W0TtWXV5O-7emlBPv5OWE3VDBN9Gr1w6&usp=sharing')
 
+def submit(name, how, message):
+    options = ', '.join(how)
+    text = f'''
+＜どうやって？＞
+{options}
+
+＜メッセージ＞
+{message}
+'''
+    requests.post('https://hemsq-event.herokuapp.com/event',
+        headers={"content-type": "application/json"},
+        data=json.dumps({'name': name, 'text': text}))
+    st.session_state.pages['TOP'].func
+
 def question():
-    st.write('アンケートにご協力お願いいたします。')
+    st.write('もしよろしければ、アンケートにお答えください！')
     with st.form('question_form'):
-        options = st.multiselect(
+        name = st.text_input('ニックネーム', placeholder='なしでも大丈夫 :)')
+        how = st.multiselect(
             'どのようにして気づかれましたか？(複数回答可)',
             options=[
                 '適当にいじってたら偶然見つけた',
@@ -28,8 +43,12 @@ def question():
                 'どれでもない',
             ],
             key='question')
+        message = st.text_area('メッセージをもらえると主が嬉しくてニヤニヤします',
+            placeholder='もちろんなしでも大丈夫 :)')
         st.form_submit_button(
             label='送信',
-            on_click=st.session_state.pages['TOP'].func)
+            on_click=submit,
+            args=[name, how, message]
+        )
 
 sukima_text = '...お？おおおおおお？'
